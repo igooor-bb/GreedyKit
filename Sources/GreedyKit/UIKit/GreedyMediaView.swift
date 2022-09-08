15d66c6e70c1bac1b91fd0171e26c91cdc0de014
Igor Belov <igooor.ww@gmail.com>
2022-09-08 23:31:38 +0300

    Fix init with coder

--- Sources/GreedyKit/UIKit/GreedyMediaView.swift
+++ Sources/GreedyKit/UIKit/GreedyMediaView.swift
@@ -33,6 +33,6 @@ public class GreedyMediaView: UIView {
     }
     
     public required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 }
